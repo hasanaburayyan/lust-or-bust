37c2976f9f6576d55b26437107f91baf701bafa5
Hasan <hasanaburayyan21@gmail.com>
2024-11-26 18:14:18 -0500

    fixed base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,8 +12,8 @@ const config: Config = {
   // Set the production url of your site here
   url: 'https://docs.lustorbust.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // For GitHub pages deployment, it is often '/<projectName>/' 
+  baseUrl: '/lust-or-bust/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
