7e4bb034bc1e08f8f289625ae2c35be652b6890c
Hasan <hasanaburayyan21@gmail.com>
2024-11-26 17:50:28 -0500

    fixed config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'lustorbust', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
